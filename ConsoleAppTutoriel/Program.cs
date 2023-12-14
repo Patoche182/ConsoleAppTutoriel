0cd4177e8f662b41d3427faad14ba7f253ede4bb
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-15 00:04:05 +0100

    ADD : Thread

--- ConsoleAppTutoriel/Program.cs
+++ ConsoleAppTutoriel/Program.cs
@@ -8,15 +8,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
             Console.WriteLine("Début du programme");
-            FonctionComplexe();
-            FonctionComplexe();
-            FonctionComplexe();
-            FonctionComplexe();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread monThread = new Thread(new ThreadStart(FonctionComplexe));
+                monThread.Start();
+            }
             Console.WriteLine("Fin du programme");
         }
 
         static void FonctionComplexe()
         {
+            Console.WriteLine("Dans le Thread : " + Thread.CurrentThread.ManagedThreadId);
             Thread.Sleep(2000); // Pause 2 secondes
             Console.WriteLine("Travail effectué !");
         }
