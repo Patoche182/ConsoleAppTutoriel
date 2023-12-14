3b140a877357a60c4141cd8bdeeba7e748405800
PatriceHANNE <patrice.hanne@gmail.com>
2023-12-14 23:52:16 +0100

    ADD : FonctionComplexe()

--- ConsoleAppTutoriel/Program.cs
+++ ConsoleAppTutoriel/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Début du programme");
+            FonctionComplexe();
+            FonctionComplexe();
+            FonctionComplexe();
+            FonctionComplexe();
+            Console.WriteLine("Fin du programme");
+        }
+
+        static void FonctionComplexe()
+        {
+            Thread.Sleep(2000); // Pause 2 secondes
+            Console.WriteLine("Travail effectué !");
+        }
+    }
+}
